c259a21ee4092042f62ab298d6dbe82882c4b5e7
Christian Alonso <christian.alonso@wizeline.com>
2023-10-31 17:13:07 -0600

    Add items form

--- src/InvoiceApp.jsx
+++ src/InvoiceApp.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { getInvoice } from "./Services/getInvoice";
 import { ClientView } from "./Components/ClientView";
 import { CompanyView } from "./Components/CompanyView";
@@ -7,8 +8,37 @@ import { ProductsView } from "./Components/ProductsView";
 import { TotalView } from "./Components/TotalView";
 
 export default function InvoiceApp() {
-
     const {id, name, client, company, items, total} = getInvoice();
+
+    // const [productValue, setProductValue] = useState('');
+    // const [priceValue, setPriceValue] = useState(0);
+    // const [quantify, setQuantify] = useState(0);
+
+    const initialState = {};
+
+    const [values, setValues] = useState(initialState);
+
+    const reset = () => {
+        setValues(initialState);
+    };
+
+    const [itemsList, setItemsList] = useState(items);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(values);
+        setItemsList([...items, values]);
+
+    }
+
+    const handleInputChange = ({target}) => {
+        setValues({
+            ...values,
+            [target.name]: target.value
+        })
+        return values;
+    };
+
 
     return (
         <>
@@ -40,9 +70,42 @@ export default function InvoiceApp() {
                         <br/>
                         <ProductsView
                             title={"Products"}
-                            items={items}
+                            items={itemsList}
                         />
                         <TotalView total={total}/>
+                        <form className="w-50" onSubmit={handleSubmit}>
+                            <input
+                                type="text"
+                                placeholder="Product..."
+                                name="product"
+                                className="form-control m-3"
+                                onChange={handleInputChange}
+                            />
+                            <input
+                                type="text"
+                                name="price"
+                                placeholder="$$$..."
+                                className="form-control m-3"
+                                onChange={handleInputChange}
+                            />
+                            <input
+                                type="text"
+                                name="quantify"
+                                placeholder="How much..."
+                                className="form-control m-3"
+                                onChange={handleInputChange}
+                            />
+                        </form>
+                        <button
+                            type="submit"
+                            className="btn btn-primary"
+                            onClick={handleSubmit}
+                        >Add item</button>
+                        <button
+                            type="reset"
+                            className="btn btn-primary m-3"
+                            onClick={reset}
+                        >Reset</button>
                     </div>
                 </div>
             </div>
